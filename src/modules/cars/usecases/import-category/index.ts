b1ac7eac33977ac5d8ae2422f9b219060c37ac41
Yan Soares <yts.1995@gmail.com>
2021-03-23 16:50:15 -0300

    chore: remove files

--- src/modules/cars/usecases/import-category/index.ts
+++
@@ -1,9 +0,0 @@
-import { CategoryRepository } from '@/modules/cars/repositories/implementations'
-import { ImportCategoryController } from './import-category-controller'
-import { ImportCategoryUseCase } from './import-category-usecase'
-
-const categoryRepository = null
-const importCategoryUseCase = new ImportCategoryUseCase(categoryRepository)
-const importCategoryController = new ImportCategoryController(importCategoryUseCase)
-
-export { importCategoryController }
--- src/modules/cars/usecases/list-categories/index.ts
+++
@@ -1,9 +0,0 @@
-import { CategoryRepository } from "@/modules/cars/repositories/implementations/";
-import { ListCategoryController } from "./list-category-controller";
-import { ListCategoryUseCase } from "./list-category-usecase";
-
-const listCatgoryRepository = null
-const listCategoryUseCase = new ListCategoryUseCase(listCatgoryRepository);
-const listCategoryController = new ListCategoryController(listCategoryUseCase);
-
-export { listCategoryController }
