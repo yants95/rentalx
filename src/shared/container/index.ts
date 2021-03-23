b645cbfaf918d9345e88b4acd6e4f2bae6d14926
Yan Soares <yts.1995@gmail.com>
2021-03-23 16:54:10 -0300

    chore: add SpecificationRepository dependency

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,13 @@
-import { ICategoryRepository, CategoryRepository } from '@/modules/cars/repositories'
+import { ICategoryRepository, CategoryRepository, ISpecificationRepository, SpecificationRepository } from '@/modules/cars/repositories'
 
 import { container } from 'tsyringe'
 
 container.registerSingleton<ICategoryRepository>(
     'CategoryRepository',
     CategoryRepository
+)
+
+container.registerSingleton<ISpecificationRepository>(
+    'SpecificationRepository',
+    SpecificationRepository
 )
