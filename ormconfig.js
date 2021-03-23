a0219d93e62f1e8247baca5c6237c065fefb2f6c
Yan Soares <yts.1995@gmail.com>
2021-03-23 10:53:36 -0300

    chore: create categories migration

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,12 @@
 module.exports = {
     "type": "postgres",
-    "host":  process.env.POSTGRES_HOST,
+    "host":  "localhost",
     "port": process.env.POSTGRES_PORT,
     "username": process.env.POSTGRES_USERNAME,
     "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DB
+    "database": process.env.POSTGRES_DB,
+    "migrations": ['./src/database/migrations/*.ts'],
+    "cli": {
+        "migrationsDir": "./src/database/migrations"
+    }
 }
