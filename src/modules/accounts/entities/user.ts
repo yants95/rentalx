7a0cc098f583ef594658c135490c1cc34a263a60
Yan Soares <yts.1995@gmail.com>
2021-03-23 20:15:33 -0300

    chore: create user migration and entity

---
+++ src/modules/accounts/entities/user.ts
@@ -0,0 +1,36 @@
+import { v4 as uuidv4 } from 'uuid'
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+
+@Entity('users')
+export class User {
+    @PrimaryColumn()
+    id: string
+
+    @Column()
+    name: string
+
+    @Column()
+    username: string
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @Column()
+    drive_license: string
+
+    @Column()
+    isAdmin: boolean
+
+    @CreateDateColumn()
+    created_at: Date
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidv4()
+            this.isAdmin = false
+        }
+    }
+}
