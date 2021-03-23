73bc1aa1d6e3cef1e7664fe14e25f17ceb1242fd
Yan Soares <yts.1995@gmail.com>
2021-03-23 16:37:07 -0300

    chore: remove files

--- src/modules/cars/usecases/create-specification/index.ts
+++
@@ -1,9 +0,0 @@
-import { CategoryRepository } from "@/modules/cars/repositories/implementations";
-import { CreateSpecificationController } from "@/modules/cars/usecases/create-specification/create-specification-controller";
-import { CreateSpecificationUseCase } from "@/modules/cars/usecases/create-specification/create-specification-usecase";
-
-const createSpecificationRepository = null
-const createSpecificationUseCase = new CreateSpecificationUseCase(createSpecificationRepository);
-const createSpecificationController = new CreateSpecificationController(createSpecificationUseCase);
-
-export { createSpecificationController }
