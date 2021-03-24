b7a220fb221ea803cd790f4bd51615ad878257b2
Yan Soares <yts.1995@gmail.com>
2021-03-24 20:04:07 -0300

    chore: add update-user-avatar migration

---
+++ src/database/migrations/1616624613324-alter-user-add-avatar.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class alterUserAddAvatar1616624613324 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn('users',
+            new TableColumn({
+                name: 'avatar_url',
+                type: 'varchar',
+                isNullable: true
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('users', 'avatar_url')
+    }
+
+}
