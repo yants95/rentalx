5d94feff59c30f3b923177984f8840207616c82c
Yan Soares <yts.1995@gmail.com>
2021-03-24 11:40:03 -0300

    refactor: change to use express-async-erros

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import 'module-alias/register'
+import 'reflect-metadata'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
 
 import { router } from './routes'
 import swaggerFile from './swagger.json'
@@ -6,7 +9,8 @@ import swaggerFile from './swagger.json'
 import './database';
 import '@/shared/container'
 
-import express from 'express'
+import { AppError } from '@/errors';
+
 import swaggerUi from 'swagger-ui-express'
 
 const app = express()
@@ -14,4 +18,16 @@ const app = express()
 app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(router)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({ message: err.message })
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: `Internal server error - ${err.message}`
+    })
+})
+
 app.listen(3333, () => console.log('Server is running!'));
