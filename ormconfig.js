4dbee6213304861595a371853bb41aa9689e65f6
Yan Soares <yts.1995@gmail.com>
2021-03-26 09:36:58 -0300

    chore: docker-compose configs

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ module.exports = {
     "type": "postgres",
     "host": process.env.POSTGRES_HOST,
     "port": process.env.POSTGRES_PORT,
-    "username": process.env.POSTGRES_USERNAME,
+    "username": process.env.POSTGRES_USER,
     "password": process.env.POSTGRES_PASSWORD,
     "database": process.env.POSTGRES_DB,
     "migrations": ['./src/database/migrations/*.ts'],
