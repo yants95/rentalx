9f8fef5761200a71004f3d3a2f455465b0b63dd6
Yan Soares <yts.1995@gmail.com>
2021-03-29 20:19:39 -0300

    chore: change server file imports

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import 'module-alias/register'
 import 'reflect-metadata'
+import 'module-alias/register'
 import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 
