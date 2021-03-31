d0a9ec8809bbc0a606b7f26ea6b173eb12a7bd00
Yan Soares <yts.1995@gmail.com>
2021-03-31 17:34:26 -0300

    feat: add car-specifications route

--- src/shared/infra/http/routes/car.routes.ts
+++ src/shared/infra/http/routes/car.routes.ts
@@ -1,16 +1,17 @@
 
-import { CreateCarController, ListAvailableCarsController }  from '@/modules/cars/usecases'
+import { CreateCarController, ListAvailableCarsController, CreateCarSpecificationController }  from '@/modules/cars/usecases'
+import { ensureAuthenticate, ensureAdmin } from '@/shared/infra/http/middlewares';
 
 import { Router } from 'express'
-import { ensureAuthenticate } from '../middlewares';
-import { ensureAdmin } from '../middlewares/ensure-admin';
 
 const carRouter = Router()
 
 const createCarController = new CreateCarController()
 const listAvailableCarsController = new ListAvailableCarsController()
+const createCarSpecificationController = new CreateCarSpecificationController()
 
 carRouter.post('/', ensureAuthenticate, ensureAdmin, createCarController.handle);
+carRouter.post('/specifications/:id', ensureAuthenticate, ensureAdmin, createCarSpecificationController.handle);
 carRouter.get('/available', listAvailableCarsController.handle);
 
 export { carRouter }
