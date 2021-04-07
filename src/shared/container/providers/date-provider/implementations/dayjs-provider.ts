a0054957d2166ea8ab7012e180257885284d5678
Yan Soares <yts.1995@gmail.com>
2021-04-06 21:14:18 -0300

    feat: add compareInDays method

--- src/shared/container/providers/date-provider/implementations/dayjs-provider.ts
+++ src/shared/container/providers/date-provider/implementations/dayjs-provider.ts
@@ -4,20 +4,26 @@ import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 
 dayjs.extend(utc)
-
 export class DayJSProvider implements IDateProvider {
-    compareInHours(start_date: Date, end_date: Date): number {
-        const endDateUTC = this.convertToUTC(end_date)
-        const startDateUTC = this.convertToUTC(start_date)
+  compareInHours(start_date: Date, end_date: Date): number {
+    const endDateUTC = this.convertToUTC(end_date)
+    const startDateUTC = this.convertToUTC(start_date)
 
-        return dayjs(endDateUTC).diff(startDateUTC, 'hours')
-    }
+    return dayjs(endDateUTC).diff(startDateUTC, 'hours')
+  }
 
-    convertToUTC(date: Date): string {
-        return dayjs(date).utc().local().format()
-    }
+  convertToUTC(date: Date): string {
+    return dayjs(date).utc().local().format()
+  }
 
-    dateNow(): Date {
-        return dayjs().toDate()
-    }
+  dateNow(): Date {
+    return dayjs().toDate()
+  }
+
+  compareInDays(start_date: Date, end_date: Date): number {
+    const endDateUTC = this.convertToUTC(end_date)
+    const startDateUTC = this.convertToUTC(start_date)
+
+    return dayjs(endDateUTC).diff(startDateUTC, 'days')
+  }
 }
