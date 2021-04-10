a2dc7c4f9bfa3f5e8f5356405db39f592eab8820
Yan Soares <yts.1995@gmail.com>
2021-04-10 18:32:18 -0300

    feat: add reset password route

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,12 +1,14 @@
 
-import { SendForgotPasswordMailController }  from '@/modules/accounts/usecases'
+import { SendForgotPasswordMailController, ResetPasswordController }  from '@/modules/accounts/usecases'
 
 import { Router } from 'express'
 
 const passwordRouter = Router()
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController()
+const resetPasswordController = new ResetPasswordController()
 
 passwordRouter.post('/forgot', sendForgotPasswordMailController.handle);
+passwordRouter.post('/reset', resetPasswordController.handle);
 
 export { passwordRouter }
