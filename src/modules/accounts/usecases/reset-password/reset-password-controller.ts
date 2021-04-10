e52fa080e953b4b93a18815cc6df79fa10bdda33
Yan Soares <yts.1995@gmail.com>
2021-04-10 18:30:53 -0300

    feat: add reset-password

---
+++ src/modules/accounts/usecases/reset-password/reset-password-controller.ts
@@ -0,0 +1,16 @@
+import { ResetPasswordUseCase } from "@/modules/accounts/usecases";
+
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+export class ResetPasswordController {
+  async handle (request: Request, response: Response): Promise<Response> {
+    const { token } = request.query
+    const { password } = request.body
+
+    const resetPasswordUseCase = container.resolve(ResetPasswordUseCase)
+    await resetPasswordUseCase.execute(String(token), password)
+    
+    return response.send()
+  }
+}
---
+++ src/modules/accounts/usecases/reset-password/reset-password-usecase.ts
@@ -0,0 +1,33 @@
+import { IUserTokenRepository, IUserRepository } from "@/modules/accounts/repositories";
+import { IDateProvider } from "@/shared/container/providers";
+import { AppError } from "@/shared/errors";
+
+import { inject, injectable } from "tsyringe"
+import { hash } from "bcryptjs"
+
+@injectable()
+export class ResetPasswordUseCase {
+  constructor (
+    @inject('UserTokenRepository')
+    private userTokenRepository: IUserTokenRepository,
+    @inject('UserRepository')
+    private userRepository: IUserRepository,
+    @inject('DayJSProvider')
+    private dateProvider: IDateProvider
+  ) {}
+
+  async execute (token: string, password: string): Promise<void> {
+    const userToken = await this.userTokenRepository.findByRefreshToken(token)
+    if (!userToken) throw new AppError('Invalid token.')
+
+    if (this.dateProvider.compareIfBefore(userToken.expires_date, this.dateProvider.dateNow())) {
+      if (!userToken) throw new AppError('Token expired.')
+    }
+
+    const user = await this.userRepository.findById(userToken.user_id)
+    user.password = await hash(password, 8)
+
+    await this.userRepository.create(user)
+    await this.userTokenRepository.deleteTokenById(userToken.id)
+  }
+}
