6383df6a50746f7967cb1e389cd41f2d4d4d4e72
Yan Soares <yts.1995@gmail.com>
2021-04-10 15:23:08 -0300

    chore: addHours method added

--- src/shared/container/providers/date-provider/implementations/dayjs-provider.ts
+++ src/shared/container/providers/date-provider/implementations/dayjs-provider.ts
@@ -30,4 +30,8 @@ export class DayJSProvider implements IDateProvider {
   addDays (days: number): Date {
     return dayjs().add(days, "days").toDate();
   }
+
+  addHours (hours: number): Date {
+    return dayjs().add(hours, "hours").toDate();
+  }
 }
