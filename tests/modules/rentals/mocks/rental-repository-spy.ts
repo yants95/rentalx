164870b596aa15df51bd99533c972d7b73e8388d
Yan Soares <yts.1995@gmail.com>
2021-04-10 10:39:40 -0300

    feat: findByUser method implemented

--- tests/modules/rentals/mocks/rental-repository-spy.ts
+++ tests/modules/rentals/mocks/rental-repository-spy.ts
@@ -3,24 +3,28 @@ import { ICreateRentDTO } from "@/modules/rentals/dtos";
 import { Rental } from "@/modules/rentals/infra/typeorm/entities";
 
 export class RentalRepositorySpy implements IRentalRepository {
-    rentals: Rental[] = []
+  rentals: Rental[] = []
 
-    async create(data: ICreateRentDTO): Promise<Rental> {
-        const rental = new Rental()
-        Object.assign(rental, { ...data, start_date: new Date('2021-01-04') })
-        this.rentals.push(rental)
-        return rental
-    }
+  async create(data: ICreateRentDTO): Promise<Rental> {
+    const rental = new Rental()
+    Object.assign(rental, { ...data, start_date: new Date('2021-01-04') })
+    this.rentals.push(rental)
+    return rental
+  }
 
-    async findOpenRentalByCar(car_id: string): Promise<Rental> {
-        return this.rentals.find(rental => rental.car_id === car_id && !rental.end_date)
-    }
+  async findOpenRentalByCar(car_id: string): Promise<Rental> {
+    return this.rentals.find(rental => rental.car_id === car_id && !rental.end_date)
+  }
 
-    async findOpenRentalByUser(user_id: string): Promise<Rental> {
-        return this.rentals.find(rental => rental.user_id === user_id && !rental.end_date)
-    }
+  async findOpenRentalByUser(user_id: string): Promise<Rental> {
+    return this.rentals.find(rental => rental.user_id === user_id && !rental.end_date)
+  }
 
-    async findById(id: string): Promise<Rental> {
-        return this.rentals.find(rental => rental.id === id)
-    }
+  async findById(id: string): Promise<Rental> {
+    return this.rentals.find(rental => rental.id === id)
+  }
+
+  async findByUser(user_id: string): Promise<Rental[]> {
+    return this.rentals.filter(rental => rental.user_id === user_id)
+  }
 }
