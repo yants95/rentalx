ad88f459383c7d4c23720c467b86c582fb332dd5
Yan Soares <yts.1995@gmail.com>
2021-04-10 11:46:01 -0300

    chore: add user-token-repository into container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,7 @@
 import { CategoryRepository, SpecificationRepository, CarRepository, CarImagesRepository } from '@/modules/cars/infra/typeorm/repositories'
 import { ICarRepository, ISpecificationRepository, ICategoryRepository, ICarImagesRepository } from '@/modules/cars/repositories'
-import { IUserRepository, UserRepository } from '@/modules/accounts/repositories'
+import { IUserRepository, IUsersTokenRepository } from '@/modules/accounts/repositories'
+import { UserRepository, UsersTokenRepository } from '@/modules/accounts/infra/typeorm/repositories'
 import { IRentalRepository } from '@/modules/rentals/repositories'
 import { RentalRepository } from '@/modules/rentals/infra/typeorm/repositories'
 
@@ -9,31 +10,36 @@ import '@/shared/container/providers'
 import { container } from 'tsyringe'
 
 container.registerSingleton<ICategoryRepository>(
-    'CategoryRepository',
-    CategoryRepository
+  'CategoryRepository',
+  CategoryRepository
 )
 
 container.registerSingleton<ISpecificationRepository>(
-    'SpecificationRepository',
-    SpecificationRepository
+  'SpecificationRepository',
+  SpecificationRepository
 )
 
 container.registerSingleton<IUserRepository>(
-    'UserRepository',
-    UserRepository
+  'UserRepository',
+  UserRepository
 )
 
 container.registerSingleton<ICarRepository>(
-    'CarRepository',
-    CarRepository
+  'CarRepository',
+  CarRepository
 )
 
 container.registerSingleton<ICarImagesRepository>(
-    'CarImagesRepository',
-    CarImagesRepository
+  'CarImagesRepository',
+  CarImagesRepository
 )
 
 container.registerSingleton<IRentalRepository>(
-    'RentalRepository',
-    RentalRepository
+  'RentalRepository',
+  RentalRepository
+)
+
+container.registerSingleton<IUsersTokenRepository>(
+  'UsersTokenRepository',
+  UsersTokenRepository
 )
