b6868cae2698074fb6f0e0a05728094f0b113e60
Yan Soares <yts.1995@gmail.com>
2021-04-10 18:31:14 -0300

    chore: add compareIfBefore method

--- src/shared/container/providers/date-provider/implementations/dayjs-provider.ts
+++ src/shared/container/providers/date-provider/implementations/dayjs-provider.ts
@@ -34,4 +34,8 @@ export class DayJSProvider implements IDateProvider {
   addHours (hours: number): Date {
     return dayjs().add(hours, "hours").toDate();
   }
+
+  compareIfBefore(start_date: Date, end_date: Date): boolean {
+    return dayjs(start_date).isBefore(end_date)
+  }
 }
