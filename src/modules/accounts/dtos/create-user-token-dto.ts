bc9054677d014a675fe76605b17e44e0b952307d
Yan Soares <yts.1995@gmail.com>
2021-04-10 11:16:41 -0300

    chore: add create-user-token-dto

---
+++ src/modules/accounts/dtos/create-user-token-dto.ts
@@ -0,0 +1,5 @@
+export interface ICreateUsersTokenDTO {
+  user_id: string
+  expires_date: string
+  refresh_token: string
+}
--- src/modules/accounts/dtos/index.ts
+++ src/modules/accounts/dtos/index.ts
@@ -1,1 +1,2 @@
-export * from './create-user.dto'
+export * from './create-user-dto'
+export * from './create-user-token-dto'
