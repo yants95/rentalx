ddfcc9e3bed806c080f29f302db369a37e4c53f6
Yan Soares <yts.1995@gmail.com>
2021-04-11 19:02:50 -0300

    fix: change token to refresh_token

--- src/modules/accounts/repositories/i-user-token-repository.ts
+++ src/modules/accounts/repositories/i-user-token-repository.ts
@@ -2,7 +2,7 @@ import { ICreateUserTokenDTO } from "@/modules/accounts/dtos";
 import { UserToken } from "@/modules/accounts/infra/typeorm/entities"
 export interface IUserTokenRepository {
   create(data: ICreateUserTokenDTO): Promise<UserToken>
-  findByUserIdAndRefreshToken(user_id: string, token: string): Promise<UserToken>
+  findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserToken>
   deleteTokenById(token_id: string): Promise<void>
   findByRefreshToken(refresh_token: string): Promise<UserToken>
 }
