51312d514960e65b096ff82c440165c993b4fc66
Yan Soares <yts.1995@gmail.com>
2021-04-11 19:05:16 -0300

    test: complete test with whole dependencies of usecase

--- tests/modules/accounts/usecases/authenticate-user-usecase.spec.ts
+++ tests/modules/accounts/usecases/authenticate-user-usecase.spec.ts
@@ -2,61 +2,68 @@
 import "reflect-metadata"
 
 import { AuthenticateUserUseCase, CreateUserUseCase } from "@/modules/accounts/usecases"
-import { UserRepositorySpy } from "@/tests/modules/accounts/mocks"
+import { UserRepositorySpy, UserTokenRepositorySpy } from "@/tests/modules/accounts/mocks"
 import { ICreateUserDTO } from "@/modules/accounts/dtos"
 import { AppError } from "@/shared/errors"
+import { DayJSProvider } from "@/shared/container/providers"
 
 type SutTypes = {
-    sut: AuthenticateUserUseCase
-    createUserUseCase: CreateUserUseCase
-    userRepositorySpy: UserRepositorySpy
+  sut: AuthenticateUserUseCase
+  createUserUseCase: CreateUserUseCase
+  userRepositorySpy: UserRepositorySpy
+  userTokenRepositorySpy: UserTokenRepositorySpy
+  dateProvider: DayJSProvider
 }
 
 const makeSut = (): SutTypes => {
     const userRepositorySpy = new UserRepositorySpy()
+    const userTokenRepositorySpy = new UserTokenRepositorySpy()
+    const dateProvider = new DayJSProvider()
     const createUserUseCase = new CreateUserUseCase(userRepositorySpy)
-    const sut = new AuthenticateUserUseCase(userRepositorySpy)
+    const sut = new AuthenticateUserUseCase(userRepositorySpy, userTokenRepositorySpy, dateProvider)
     return {
       sut,
       userRepositorySpy,
-      createUserUseCase
+      createUserUseCase,
+      userTokenRepositorySpy,
+      dateProvider
     }
 }
 
 const makeUser = (): ICreateUserDTO => ({
-    driver_license: 'any_driver_license',
-    email: 'any_email@mail.com',
-    password: 'any_passwprd',
-    name: 'any_user'
+  driver_license: 'any_driver_license',
+  email: 'any_email@mail.com',
+  password: 'any_passwprd',
+  name: 'any_user'
 })
 
 describe('Authenticate User', () => {
-    it('should be able to authenticate user', async () => {
-        const { sut, createUserUseCase } = makeSut()
-        const user = makeUser()
+  it('should be able to authenticate user', async () => {
+    const { sut, createUserUseCase } = makeSut()
+    const user = makeUser()
 
-        await createUserUseCase.execute(makeUser())
-        const response = await sut.execute(user.email, user.password)
+    await createUserUseCase.execute(makeUser())
+    const response = await sut.execute(user.email, user.password)
 
-        expect(response).toHaveProperty('token')
-    })
+    expect(response).toHaveProperty('token')
+  })
 
-    it('should not be able to authenticate a non existent user', () => {
-        const { sut } = makeSut()
-        const user = makeUser()
+  it('should not be able to authenticate a non existent user', () => {
+    const { sut } = makeSut()
+    const user = makeUser()
 
-        expect(async() => {
-            await sut.execute(user.email, user.password);
-        }).rejects.toBeInstanceOf(AppError)
-    })
+    expect(async() => {
+        await sut.execute(user.email, user.password);
+    }).rejects.toBeInstanceOf(AppError)
+  })
 
-    it('shout not be able to authenticate with incorrect password', () => {
-        const { sut, createUserUseCase } = makeSut()
-        const user = makeUser()
+  it('shout not be able to authenticate with incorrect password', () => {
+    const { sut, createUserUseCase } = makeSut()
+    const user = makeUser()
 
-        expect(async() => {
-            await createUserUseCase.execute(user);
-            await sut.execute(user.email, 'incorrect_password')
-        }).rejects.toBeInstanceOf(AppError)
-    })
+    expect(async() => {
+        await createUserUseCase.execute(user);
+        await sut.execute(user.email, 'incorrect_password')
+    }).rejects.toBeInstanceOf(AppError)
+  })
 })
