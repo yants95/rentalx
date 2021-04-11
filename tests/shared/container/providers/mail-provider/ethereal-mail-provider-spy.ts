6ad33da0e7e56a8596c8a0536129c9aa8904c379
Yan Soares <yts.1995@gmail.com>
2021-04-11 19:31:18 -0300

    test: add ethereal-mail-provider-spy

---
+++ tests/shared/container/providers/mail-provider/ethereal-mail-provider-spy.ts
@@ -0,0 +1,9 @@
+import { IMailProvider } from "@/shared/container/providers";
+
+export class EtherealMailProviderSpy implements IMailProvider {
+  private message: any[] = []
+
+  async sendMail(to: string, subject: string, variables: any, path: string): Promise<void> {
+    this.message.push(to, subject, variables, path)
+  }
+}
