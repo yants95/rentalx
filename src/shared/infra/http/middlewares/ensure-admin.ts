64517121473738b9bb36f8ecfd845eb256a6fa34
Yan Soares <yts.1995@gmail.com>
2021-04-11 19:04:39 -0300

    chore: lint code

--- src/shared/infra/http/middlewares/ensure-admin.ts
+++ src/shared/infra/http/middlewares/ensure-admin.ts
@@ -1,14 +1,14 @@
-import { UserRepository } from "@/modules/accounts/repositories";
+import { UserRepository } from "@/modules/accounts/infra/typeorm/repositories";
 import { AppError } from "@/shared/errors";
 
 import { NextFunction, Request, Response } from "express";
 
 export async function ensureAdmin(request: Request, _: Response, next: NextFunction) {
-    const { id } = request.user;
-    const userRepository = new UserRepository()
-    const user = await userRepository.findById(id)
+  const { id } = request.user;
+  const userRepository = new UserRepository()
+  const user = await userRepository.findById(id)
 
-    if (!user.isAdmin) throw new AppError('User is not an admin!')
+  if (!user.isAdmin) throw new AppError('User is not an admin!')
 
-    return next()
+  return next()
 }
--- src/shared/infra/http/middlewares/ensure-authenticate.ts
+++ src/shared/infra/http/middlewares/ensure-authenticate.ts
@@ -9,23 +9,23 @@ interface IPayload {
 }
 
 export async function ensureAuthenticate(request: Request, _: Response, next: NextFunction) {
-    const authHeader = request.headers.authorization;
-    if (!authHeader) throw new AppError('Token is missing', 401)
+  const authHeader = request.headers.authorization;
+  if (!authHeader) throw new AppError('Token is missing', 401)
 
-    const [, token] = authHeader.split(' ')
+  const [, token] = authHeader.split(' ')
+  
+  try {
+    const { sub: user_id } = verify(token, process.env.JWT_SECRET) as IPayload;
+
+    const userRepository = new UserRepository()
+    const userExists = await userRepository.findById(user_id)
+
+    if (!userExists) throw new AppError('User not found!', 401)
+
+    request.user = { id: user_id }
     
-    try {
-        const { sub: user_id } = verify(token, process.env.JWT_SECRET) as IPayload;
-
-        const userRepository = new UserRepository()
-        const userExists = await userRepository.findById(user_id)
-
-        if (!userExists) throw new AppError('User not found!', 401)
-
-        request.user = { id: user_id }
-        
-        next()
-    } catch {
-        throw new AppError('Invalid token!', 401)
-    }
+    next()
+  } catch {
+    throw new AppError('Invalid token!', 401)
+  }
 }
