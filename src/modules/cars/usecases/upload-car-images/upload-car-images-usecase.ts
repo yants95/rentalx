602be3fd17b7d112432695abcb1fa9e82987ef3d
Yan Soares <yts.1995@gmail.com>
2021-04-12 11:05:50 -0300

    feat: integrate upload with storage-provider

--- src/modules/cars/usecases/upload-car-images/upload-car-images-usecase.ts
+++ src/modules/cars/usecases/upload-car-images/upload-car-images-usecase.ts
@@ -1,15 +1,20 @@
 import { ICarImagesRepository } from "@/modules/cars/repositories";
+import { IStorageProvider } from "@/shared/container/providers";
 
 import { inject, injectable } from 'tsyringe'
-
 @injectable()
 export class UploadCarImagesUseCase {
-    constructor (
-        @inject('CarImagesRepository')
-        private carImagesRepository: ICarImagesRepository
-    ) {}
+  constructor (
+    @inject('CarImagesRepository')
+    private carImagesRepository: ICarImagesRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
+  ) {}
 
-    async execute(car_id: string, images_name: string[]): Promise<void> {
-        images_name.map(async image => await this.carImagesRepository.create(car_id, image))
-    }
+  async execute(car_id: string, images_name: string[]): Promise<void> {
+    images_name.map(async image => {
+      await this.carImagesRepository.create(car_id, image)
+      await this.storageProvider.save(image, "cars")
+    })
+  }
 }
