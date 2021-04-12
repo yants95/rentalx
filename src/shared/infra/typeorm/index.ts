f0b6abd1cf0e4d0f57b44266b5812f08475eb8b7
Yan Soares <yts.1995@gmail.com>
2021-04-12 20:10:40 -0300

    refactor: remove host overwrite

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -4,8 +4,6 @@ interface IOptions {
 }
 
 getConnectionOptions().then(options => {
-  const newOptions = options as IOptions;
-  newOptions.host = 'database_ignite';
   createConnection({
     ...options,
   });
