27220b54accae3a558e99b682a000deb23bad38f
Yan Soares <yts.1995@gmail.com>
2021-04-13 13:55:44 -0300

    chore: add sentry

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -15,10 +15,24 @@ import { rateLimiter } from "@/shared/infra/http/middlewares"
 
 import swaggerUi from "swagger-ui-express"
 import express, { NextFunction, Request, Response } from "express"
+// import * as Sentry from "@sentry/node"
+// import * as Tracing from "@sentry/tracing"
 
 const app = express()
 
 app.use(rateLimiter)
+
+// Sentry.init({
+//   dsn: process.env.SENTRY_DSN,
+//   integrations: [
+//     new Sentry.Integrations.Http({ tracing: true }),
+//     new Tracing.Integrations.Express({ app })
+//   ],
+//   tracesSampleRate: 1.0
+// })
+
+// app.use(Sentry.Handlers.requestHandler())
+// app.use(Sentry.Handlers.tracingHandler())
 
 app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocs))
@@ -27,6 +41,8 @@ app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`))
 app.use('/car', express.static(`${upload.tmpFolder}/cars`))
 
 app.use(router)
+
+// app.use(Sentry.Handlers.errorHandler())
 
 app.use((err: Error, _: Request, response: Response, __: NextFunction) => {
     if (err instanceof AppError) {
