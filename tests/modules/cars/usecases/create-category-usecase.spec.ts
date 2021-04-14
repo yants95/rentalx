333656bc71cb7f58c1f57158f9fddda8f3afc1e6
Yan Soares <yts.1995@gmail.com>
2021-04-14 09:49:44 -0300

    refactor: improve create-category-usecase test code legibility

--- tests/modules/cars/usecases/create-category-usecase.spec.ts
+++ tests/modules/cars/usecases/create-category-usecase.spec.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 
 import { CreateCategoryUseCase } from '@/modules/cars/usecases/create-category/create-category-usecase'
 import { CategoryRepositorySpy } from '@/tests/modules/cars/repositories/mocks'
+import { ICreateCategoryDTO } from '@/modules/cars/dtos'
 import { AppError } from '@/shared/errors'
 
 type SutTypes = {
@@ -18,43 +19,25 @@ const makeSut = (): SutTypes => {
   }
 }
 
+const makeCategory = (): ICreateCategoryDTO => ({
+  name: 'any_name',
+  description: 'any_description'
+})
+
 describe('CreateCategoryUseCase', () => {
   it('should be able to create a new category', async () => {
     const { sut, categoryRepositorySpy } = makeSut()
-
-    const category = {
-      name: 'any_name',
-      description: 'any_description'
-    }
-
-    await sut.execute({
-      name: category.name,
-      description: category.description
-    })
+    const category = makeCategory()
+    await sut.execute(category)
 
     const categoryCreated = await categoryRepositorySpy.findByName(category.name)
-
     expect(categoryCreated).toHaveProperty('id')
   })
 
   it('should not be able to create a new category with name already exists', async () => {
     const { sut } = makeSut()
-
-    expect(async () => {
-      const category = {
-        name: 'any_name',
-        description: 'any_description'
-      }
-
-      await sut.execute({
-        name: category.name,
-        description: category.description
-      })
-
-      await sut.execute({
-        name: category.name,
-        description: category.description
-      })
-    }).rejects.toBeInstanceOf(AppError)
+    const category = makeCategory()
+    await sut.execute({ name: category.name, description: category.description })
+    await expect(sut.execute({ name: category.name, description: category.description })).rejects.toBeInstanceOf(AppError)
   })
 })
